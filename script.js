060a5c9937ae52be8f08b9d526be4f673647a796
Satsangtech <166884263+Satsangtech@users.noreply.github.com>
2024-04-13 16:51:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', () => {
   function fetchAndDisplayQuestions() {
     const name = document.getElementById('name').value;
   const email = document.getElementById('email').value;
-  fetch(`/api/questions?name=${encodeURIComponent(name)}&email=${encodeURIComponent(email)}`)
+  fetch(`https://satsangtech.github.io/aptitude/server.js/api/questions?name=${encodeURIComponent(name)}&email=${encodeURIComponent(email)}`)
       .then(response => response.json())
       .then(data => {
         const questions = data.questions;
